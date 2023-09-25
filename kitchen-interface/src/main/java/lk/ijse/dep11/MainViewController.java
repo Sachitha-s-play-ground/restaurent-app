627b4bac7433f22544329dc60ff065a92d010282
Janani Kalpana <janani.kalpana1997@gmail.com>
2023-09-25 18:42:51 +0530

    Add kitchen interface

---
+++ kitchen-interface/src/main/java/lk/ijse/dep11/MainViewController.java
@@ -0,0 +1,20 @@
+package lk.ijse.dep11;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
+public class MainViewController {
+    public AnchorPane root;
+    public TableView tbvOrderDetails;
+    public TextField txtID;
+    public TableView tbvItemDetails;
+    public Button btnComplete;
+
+
+
+    public void btnCompleteOnAction(ActionEvent actionEvent) {
+    }
+}
