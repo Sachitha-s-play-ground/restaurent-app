2376699d284bb2b4543b9a7350c7491d636af7bc
Janani Kalpana <janani.kalpana1997@gmail.com>
2023-09-25 19:21:28 +0530

    set spinners

--- cashier-app/src/main/java/ijse/lk/dep11/CashierViewController.java
+++ cashier-app/src/main/java/ijse/lk/dep11/CashierViewController.java
@@ -2,27 +2,26 @@ package ijse.lk.dep11;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.Spinner;
-import javafx.scene.control.TableView;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 
 public class CashierViewController {
+
+
 
     @FXML
     private Button btnOrder;
 
     @FXML
-    private Spinner<?> spnBurger;
+    private Spinner<Integer> spnBurger;
 
     @FXML
-    private Spinner<?> spnChicken;
+    private Spinner<Integer> spnChicken;
 
     @FXML
-    private Spinner<?> spnFish;
+    private Spinner<Integer> spnFish;
 
     @FXML
-    private Spinner<?> spnSubmarin;
+    private Spinner<Integer> spnSubmarin;
 
     @FXML
     private TableView<?> tblCashier;
@@ -38,6 +37,21 @@ public class CashierViewController {
 
     @FXML
     void btnOrder(ActionEvent event) {
+
+    }
+
+    public void initialize(){
+        SpinnerValueFactory<Integer> valueFactory1=new SpinnerValueFactory.IntegerSpinnerValueFactory(1,100,1);
+                spnBurger.setValueFactory(valueFactory1);
+
+        SpinnerValueFactory<Integer> valueFactory2=new SpinnerValueFactory.IntegerSpinnerValueFactory(1,100,1);
+            spnChicken.setValueFactory(valueFactory2);
+
+        SpinnerValueFactory<Integer> valueFactory3=new SpinnerValueFactory.IntegerSpinnerValueFactory(1,100,1);
+            spnFish.setValueFactory(valueFactory3);
+
+        SpinnerValueFactory<Integer> valueFactory4=new SpinnerValueFactory.IntegerSpinnerValueFactory(1,100,1);
+            spnSubmarin.setValueFactory(valueFactory4);
 
     }
 
